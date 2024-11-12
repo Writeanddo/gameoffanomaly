f4d033586f1898f48a3c06a3e74f158116db62f5
Enweave <enweave@gmail.com>
2024-11-12 22:03:13 +0500

    Combat feature definition

---
+++ Source/SecretGGame/Combat/FeatureBase.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FeatureBase.h"
+
+
+// Sets default values
+AFeatureBase::AFeatureBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFeatureBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFeatureBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/SecretGGame/Combat/FeatureBase.h
@@ -0,0 +1,68 @@
+﻿// Base class for weapons and abilities.
+// Handles cooldowns, cost and regeneration.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FeatureBase.generated.h"
+
+UCLASS()
+class SECRETGGAME_API AFeatureBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFeatureBase();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (ToolTip = "If true, fires only once after activation, otherwise fires continuously."))
+	bool bIsOneShot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (ToolTip = "Total charge capacity."))
+	float FCapacity;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (ToolTip = "Cost per activation."))
+	float FCost;
+
+	// cooldown in seconds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (ToolTip = "Cooldown in seconds."))
+	float FCooldown;
+
+	// regen in seconds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (ToolTip = "Regeneration in seconds."))
+	float FRegen;
+
+	
+	
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+	bool bIsOnCooldown;
+	// Remaining charge
+	float FCurrentCharge;
+
+	// current target Actor
+	AActor* TargetActor;
+
+	// current target location
+	FVector TargetLocation;
+
+	// has target?
+	bool bHasTarget;
+	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Start feature activation and return true if successful
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	bool Activate();
+
+	//
+	// Set target location and actor
+	
+};
