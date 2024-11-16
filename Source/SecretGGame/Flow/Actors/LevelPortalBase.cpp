fa926385098ba8e696dc1e1e2d71388c8b828059
Enweave <enweave@gmail.com>
2024-11-16 06:45:27 +0500

    Anomaly example + scanner interaction

---
+++ Source/SecretGGame/Flow/Actors/LevelPortalBase.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelPortalBase.h"
+
+#include "AnomalyBase.h"
+#include "EngineUtils.h"
+#include "SecretGGame/Flow/FLowController.h"
+
+
+// Sets default values
+ALevelPortalBase::ALevelPortalBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (bAutoFillAnomalies)
+	{
+		FillAnomalies();
+	}
+	AssignAnomalies();
+}
+
+// Called when the game starts or when spawned
+void ALevelPortalBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ALevelPortalBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALevelPortalBase::Unlock()
+{
+	bLocked = false;
+	OnLevelPortalUnlocked.Broadcast();
+}
+
+void ALevelPortalBase::EnterPortal()
+{
+	if (bLocked)
+	{
+		return;
+	}
+
+	if (NextLevel != ELevelNames::None)
+	{
+		UFLowController::LoadLevel(NextLevel);
+	}
+}
+
+
+void ALevelPortalBase::AssignAnomalies()
+{
+	// go through all Anomalies and assign this portal to them
+	for (AAnomalyBase* Anomaly : Anomalies)
+	{
+		Anomaly->LevelPortal = this;
+	}
+}
+
+void ALevelPortalBase::FillAnomalies()
+{
+	// if autofill is enabled, fill anomalies array with all anomalies in level
+	if (bAutoFillAnomalies)
+	{
+		for (TActorIterator<AAnomalyBase> AnomalyItr(GetWorld()); AnomalyItr; ++AnomalyItr)
+		{
+			AAnomalyBase* Anomaly = *AnomalyItr;
+			Anomalies.Add(Anomaly);
+		}
+	}
+}
+
+
+void ALevelPortalBase::RemoveAnomaly(AAnomalyBase* Anomaly)
+{
+	Anomalies.Remove(Anomaly);
+	if (Anomalies.Num() == 0)
+	{
+		Unlock();
+	}
+}
---
+++ Source/SecretGGame/Flow/Actors/LevelPortalBase.h
@@ -0,0 +1,66 @@
+﻿// Portal to another level
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SecretGGame/Flow/LevelNames.h"
+#include "LevelPortalBase.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLevelPortalUnlocked);
+
+UCLASS()
+class SECRETGGAME_API ALevelPortalBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ALevelPortalBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Next level
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LevelPortal")
+	ELevelNames NextLevel = ELevelNames::None;
+
+	// Locked?
+	UPROPERTY(BlueprintReadOnly, Category = "LevelPortal")
+	bool bLocked = false;
+
+	// Anomalies required to unlock
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LevelPortal")
+	TArray<class AAnomalyBase*> Anomalies;
+
+	// Autofill with all anomalies in level?
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LevelPortal")
+	bool bAutoFillAnomalies = false;
+
+	// Unlock portal
+	UFUNCTION(BlueprintCallable, Category = "LevelPortal")
+	void Unlock();
+
+	// On portal enter
+	UFUNCTION(BlueprintCallable, Category = "LevelPortal")
+	void EnterPortal();
+	
+	// On unlock notification
+	UPROPERTY(BlueprintAssignable, Category = "LevelPortal")
+	FOnLevelPortalUnlocked OnLevelPortalUnlocked;
+	
+	// assign itself to anomalies in array
+	void AssignAnomalies();
+
+	// fill anomalies array with all anomalies in level
+	void FillAnomalies();
+
+	// Remove anomaly from array and check if portal can be unlocked
+	void RemoveAnomaly(AAnomalyBase* Anomaly);
+};
