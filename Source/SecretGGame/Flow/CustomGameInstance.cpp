71c339ffff6593f56827dc6c6cecf4216cc7b5aa
Enweave <enweave@gmail.com>
2024-11-29 00:14:29 +0500

    Boss keys and quest display

--- Source/SecretGGame/Flow/CustomGameInstance.cpp
+++ Source/SecretGGame/Flow/CustomGameInstance.cpp
@@ -2,3 +2,63 @@
 
 
 #include "CustomGameInstance.h"
+
+#include "GameplayState.h"
+
+
+void UCustomGameInstance::SetCurrentObjectiveId(const EQuestObjectiveIds ObjectiveId) const
+{
+	GameplayState->CurrentObjectiveId = ObjectiveId;
+	OnGamePlayStateChanged.Broadcast();
+}
+
+void UCustomGameInstance::AddBossKey(const EBossKeyNames BossKeyName) const
+{
+	GameplayState->AcquiredBossKeys.AddUnique(BossKeyName);
+
+	if (GameplayState->CurrentObjectiveId == EQuestObjectiveIds::Explore)
+	{
+		SetCurrentObjectiveId(EQuestObjectiveIds::FindKeys);
+	}
+	
+	if (HasAllKeys() && GameplayState->CurrentObjectiveId == EQuestObjectiveIds::FindKeys)
+	{
+		SetCurrentObjectiveId(EQuestObjectiveIds::DefeatBoss);
+	}
+
+	OnGamePlayStateChanged.Broadcast();
+}
+
+bool UCustomGameInstance::HasAllKeys() const
+{
+	return GameplayState->AcquiredBossKeys.Num() == GNum_Boss_Keys;
+}
+
+void UCustomGameInstance::ResetOnGameStart() const
+{
+	GameplayState->PreviousLevel = ELevelNames::None;
+	GameplayState->CurrentLevel = ELevelNames::None;
+	GameplayState->CurrentObjectiveId = EQuestObjectiveIds::Explore;
+	GameplayState->AcquiredBossKeys.Empty();
+	OnGamePlayStateChanged.Broadcast();
+}
+
+void UCustomGameInstance::SetCurrentLevel(const ELevelNames LevelName) const
+{
+	GameplayState->CurrentLevel = LevelName;
+	OnGamePlayStateChanged.Broadcast();
+}
+
+void UCustomGameInstance::UpdatePreviousLevel() const
+{
+	GameplayState->PreviousLevel = GameplayState->CurrentLevel;
+	OnGamePlayStateChanged.Broadcast();
+}
+
+void UCustomGameInstance::Init()
+{
+	Super::Init();
+	
+	// Create an instance of UGameplayState
+	GameplayState = NewObject<UGameplayState>(this, UGameplayState::StaticClass());
+}
---
+++ Source/SecretGGame/Flow/GameplayState.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LevelNames.h"
+#include "QuestObjective.h"
+#include "Actors/BossKey.h"
+#include "UObject/Object.h"
+#include "GameplayState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECRETGGAME_API UGameplayState : public UObject
+{
+	GENERATED_BODY()
+public:
+
+	// Previous level
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FlowController")
+	ELevelNames PreviousLevel = ELevelNames::None;
+
+	// Current level
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FlowController")
+	ELevelNames CurrentLevel = ELevelNames::None;
+
+	// Acquired boss keys
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FlowController")
+	TArray<EBossKeyNames> AcquiredBossKeys = {};
+
+	// Current objective id
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FlowController")
+	EQuestObjectiveIds CurrentObjectiveId = EQuestObjectiveIds::Explore;
+};
